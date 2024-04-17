2c5d144f2b804112f2b9d7b344f90a60fa327472
Abdul-Haseeb360 <166996711+Abdul-Haseeb360@users.noreply.github.com>
2024-04-18 01:35:49 +0500

    Create A simple calculater

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter first Number", type: "number", name: "firstNumber" },
+  { message: "Enter second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select the on of the oprater to perfom action ",
+    type: "list",
+    name: "operator",
+    choices: ["+", "-", "*", "/"],
+  },
+]);
+
+if (answer.operator == "+") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator == "-") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator == "*") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator == "/") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please select the valid operator");
+}
